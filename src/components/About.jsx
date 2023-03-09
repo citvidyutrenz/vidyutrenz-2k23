046a9d7d38f5edf221295d5fda05d510fc433fd1
HARISH-MADDMAXX <fwdtoharish@gmail.com>
2023-03-09 15:23:40 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,44 @@ import React from 'react'
 import { Footer } from './Footer'
 import { Location } from './Location'
 import NavBar from './NavBar'
-
+import { useState ,useEffect } from 'react'
+import {motion} from "framer-motion"
+import { Banner } from './Banner'
+import { Skills } from './Skills'
+import Loader from './Loader'
 function About() {
+
+  const [showDiv, setShowDiv] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowDiv(true);
+    }, 8500);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
   return (
     <div>
-        <NavBar />
-
-
-        <Location />
-        <Footer />
-    </div>
+    {showDiv ? (
+      <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
+        <div className="App overflow-x-hidden">
+          <NavBar />
+          <Banner />
+          <Skills />
+          {/* <Projects /> */}
+          <Location />
+          <Footer />
+        </div>
+      </motion.div>
+    ) : (
+      <div
+        className="w-100 mt-5 d-flex align-items-center justify-content-sm-center"
+        style={{ minHeight: "90vh" }}
+      >
+        <Loader />
+      </div>
+    )}
+  </div>
   )
 }
 
